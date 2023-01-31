5effe087d7567658802bbffac9e7892262effbfe
michoball <myunghun0114@gmail.com>
2023-01-31 17:54:19 +0900

    feat : Apply react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.styles.scss";
+import { useAppDispatch } from "./app/hooks";
 import ListContainer from "./components/list/ListContainer";
 import Search from "./components/search/Search";
+import { setProducts } from "./features/product/productSlice";
+import useProductQuery from "./hooks/queries/useGetProductQuery";
 
 function App() {
+  const { useGetAllProductQuery } = useProductQuery();
+  const { data: allProducts } = useGetAllProductQuery();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (allProducts) dispatch(setProducts(allProducts));
+  }, [allProducts, dispatch]);
+
   return (
     <div className="App">
       <Search />
-      <article className="searched-data">
-        <p>검색된 데이터 : 100 건</p>
-      </article>
       <ListContainer />
     </div>
   );
