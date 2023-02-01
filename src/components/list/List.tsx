a67da5ba7f6891d9dbd21c71089dfe57faa20b9f
michoball <myunghun0114@gmail.com>
2023-02-02 02:47:09 +0900

    chore : Delete comment

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -20,29 +20,15 @@ const List: React.FC<ListProps> = ({
   rating,
   stock,
 }) => {
-  // const descriptionRef = useRef<HTMLParagraphElement | null>(null);
-  // useEffect(() => {
-  //   if (description) {
-  //     const descriptionStyle = descriptionRef.current as HTMLParagraphElement;
-  //     const currentDescriptionWidth =
-  //       descriptionStyle.getBoundingClientRect().width;
-  //     descriptionStyle.style.width = `${Math.ceil(currentDescriptionWidth)}px`;
-  //     descriptionStyle.innerText = description;
-  //   }
-  // }, []);
-
   return (
     <li className="list-wrapper">
       <span className="id">{productId}</span>
       <span className="title">{productTitle}</span>
       <span className="brand">{brand}</span>
-      {/* 상품 내용은 40글자가 넘으면 ...으로 표현 css 에서 ellipsis적용 */}
       <span className="description">
-        {/* <p ref={descriptionRef}> */}
         {description.length > 40
-          ? description.slice(0, 40) + "..."
+          ? `${description.slice(0, 40)}...`
           : description}
-        {/* </p> */}
       </span>
       <span className="price">${price}</span>
       <span className="rating">{rating}</span>
