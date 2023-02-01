cd1d1ec0db0e514f115051169e66a200a0e2cb5f
michoball <myunghun0114@gmail.com>
2023-02-02 02:36:09 +0900

    feat : Add pagination button focus effect

--- src/components/pagination/PaginationItem.tsx
+++ src/components/pagination/PaginationItem.tsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./PaginationItem.styles.scss";
 
 interface PaginationItemProps {
   pageNumber: number;
+  currentPage: number;
   pageClickHandler: () => void;
 }
 
 const PaginationItem: React.FC<PaginationItemProps> = ({
   pageNumber,
+  currentPage,
   pageClickHandler,
 }) => {
+  const pageNumberRef = useRef<HTMLLIElement | null>(null);
+
+  useEffect(() => {
+    if (pageNumberRef.current) {
+      if (pageNumberRef.current.value === currentPage) {
+        pageNumberRef.current.classList.add("focus");
+      } else {
+        pageNumberRef.current.classList.remove("focus");
+      }
+    }
+  }, [currentPage]);
+
   return (
-    <li className="page-item" onClick={pageClickHandler}>
+    <li
+      className="page-item"
+      onClick={pageClickHandler}
+      value={pageNumber}
+      ref={pageNumberRef}
+    >
       <span>{pageNumber}</span>
     </li>
   );
