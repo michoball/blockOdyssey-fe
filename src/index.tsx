a11134752482bc20aa49c2689a260ec6922be3a3
michoball <myunghun0114@gmail.com>
2023-02-02 14:29:57 +0900

    chore : Delete comment and react query devtool

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
@@ -15,7 +14,6 @@ const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools />
       <Provider store={store}>
         <App />
       </Provider>
